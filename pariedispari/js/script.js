fbc3b4f57eacda8eb3015b5903bbdcf5140af666
E-Lop <edmondolopez@gmail.com>
2022-06-13 14:35:19 +0200

    aggiunta richiesta input utente

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -1,5 +1,17 @@
 /* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando 
+una funzione).
 Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Stabiliamo se la somma dei due numeri è pari o dispari (usando 
+una funzione)
 Dichiariamo chi ha vinto. */
+
+// variabile che salva scelta utente tra pari e dispari
+const userBet = prompt('Scegli: pari o dispari?');
+
+// variabile che salva il numero giocato dall'utente
+const userNumber = parseInt(prompt('Scrivi un numero da 1 a 5'));
+
+// todo test
+console.log('scelta utente:', userBet);
+console.log('numero utente:', userNumber);
